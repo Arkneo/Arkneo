33adeb2ad8367d3d88ba75add8aea951b787f6ec
Telokis <ninetainedo@gmail.com>
2017-03-13 04:26:30 +0100

    Ajout d'un timer

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "STL.hh"
 
 #include <string>
+#include <chrono>
 
 int main(int ac, char** av)
 {
@@ -13,8 +14,17 @@ int main(int ac, char** av)
   if (ac > 1)
     filePath = av[1];
 
+  auto tStart = std::chrono::high_resolution_clock::now();
   STL stl(filePath);
+  auto tEnd = std::chrono::high_resolution_clock::now();
+  std::cout << "Parsing done in " <<
+    std::chrono::duration_cast<std::chrono::milliseconds>(tEnd - tStart).count() << "ms.\n";
+  tStart = std::chrono::high_resolution_clock::now();
   stl.setupNeighbors();
-  
+  tEnd = std::chrono::high_resolution_clock::now();
+  std::cout << "Neighbors set up in " <<
+    std::chrono::duration_cast<std::chrono::milliseconds>(tEnd - tStart).count() << "ms.\n";
+
+  std::cin.get();
   return 0;
 }
